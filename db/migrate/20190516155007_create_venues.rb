649434dbad30d68f1c2ccffab675fb48bae0829c
Catherine Nguyen Cat <catanhtho@gmail.com>
2019-05-16 17:47:26 +0100

    schema updated

--- db/migrate/20190516155007_create_venues.rb
+++ db/migrate/20190516155007_create_venues.rb
@@ -1,6 +1,11 @@
 class CreateVenues < ActiveRecord::Migration[5.2]
   def change
     create_table :venues do |t|
+      t.string :name
+      t.string :location
+      t.string :type
+      t.string :description
+      t.integer :rating
 
       t.timestamps
     end
