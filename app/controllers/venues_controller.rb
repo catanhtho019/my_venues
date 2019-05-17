8b3557ea789153d6b4573ebf70f4e16825a86d6b
Reid Campbell <reidscottcampbell@gmail.com>
2019-05-17 14:14:03 +0100

    connected a few controllers

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,2 +1,30 @@
 class VenuesController < ApplicationController
+  def index
+    @venues = Venue.all
+  end
+
+  def show
+    @venue = Venue.find(params[:id])
+    # @booking = Booking.new
+    # @review = Review.new
+  end
+
+  def new
+    @venue = Venue.new
+  end
+
+  def create
+    @venue = Venue.new(venue_params)
+    if @venue.save
+      redirect_to venue_path(@venue)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def venue_params
+    params.require(:venue).permit(:name, :location, :type, :description)
+  end
 end
