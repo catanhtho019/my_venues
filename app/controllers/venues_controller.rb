9b72c5d12d6397bfd2306137ee9182324f1b862b
Reid Campbell <reidscottcampbell@gmail.com>
2019-05-17 16:00:12 +0100

    updated profile page, added add button and edit buttons

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -13,18 +13,33 @@ class VenuesController < ApplicationController
     @venue = Venue.new
   end
 
+  def edit
+    @venue = Venue.find(params[:id])
+  end
+
   def create
     @venue = Venue.new(venue_params)
     if @venue.save
-      redirect_to venue_path(@venue)
+      redirect_to venue_path(@venue), notice: 'Venue was successfully added.'
     else
       render 'new'
     end
   end
 
+  def update
+    @venue = Venue.find(params[:id])
+
+      if @venue.update(venue_params)
+        redirect_to @venue, notice: 'Venue was successfully updated.'
+      else
+        format.html { render :edit }
+      end
+    end
+
+
   private
 
   def venue_params
-    params.require(:venue).permit(:name, :location, :type, :description)
+    params.require(:venue).permit(:name, :location, :category, :description)
   end
 end
