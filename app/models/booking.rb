427be472139059c32376fdaef1a1058bc5674a5e
Reid Campbell <reidscottcampbell@gmail.com>
2019-05-20 14:52:45 +0100

    added the first attempt at booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,5 +4,5 @@ class Booking < ApplicationRecord
   has_one :review
   validates :checkin_on, presence: true
   validates :checkout_on, presence: true
-  # validates :status, inclusion: { in: ["Pending guest request", "Pending host validation", "Confirmed", "Canceled"], allow_nil: true }
+  validates :status, inclusion: { in: ["Pending guest request", "Pending host validation", "Confirmed", "Canceled"], allow_nil: true }
 end
