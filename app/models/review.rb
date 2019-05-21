f1075902b260afe54a3353264654ee1c2c492718
Lavert Ross <lross911@gmail.com>
2019-05-21 11:08:14 +0100

    made minimal changes to models

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,5 @@
 class Review < ApplicationRecord
+  belongs_to :venue
+  belongs_to :user
+  validates :rating, inclusion: { in: 0..5 }
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,2 +1,6 @@
 class Venue < ApplicationRecord
+  has_many :bookings
+  has_many :reviews
+  belongs_to :user
+  validates :category, inclusion: { in: %(bar, pubs, restaurants, clubs) } # @categories
 end
